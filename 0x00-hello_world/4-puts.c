cd48c9ab95d758b1ea3108ef23bba65cf6ac81ff
Regina Ndze <ndzekay@gmail.com>
2022-09-08 14:32:23 -0700

    Betty s standards

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-/*
- * main - Entry point
+/**
+ * main -entry point
  *
- * Return : Always 0 (Success)
+ * Return: Always 0 (success)
  *
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
