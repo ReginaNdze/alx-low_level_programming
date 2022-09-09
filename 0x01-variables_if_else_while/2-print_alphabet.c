bb00f2ce04716cd411bd59057cec6ef34aa71f44
Regina Ndze <ndzekay@gmail.com>
2022-09-09 12:42:05 -0700

    Print alphabests

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *
+  *Return: Always 0 (success)
+  */
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	return (0);
+}
+
