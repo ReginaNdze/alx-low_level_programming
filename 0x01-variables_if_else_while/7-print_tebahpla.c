a8628f86fd9160be02f9bf8d7fd7c3f5caa71186
Regina Ndze <ndzekay@gmail.com>
2022-09-11 08:37:14 -0700

    Printing alphabet in reverse with putchar function

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *
+  *Return: Always 0 (success)
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
