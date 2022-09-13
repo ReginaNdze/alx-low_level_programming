4e6863a4bafee670bbb43738f396211abb613fad
Regina Ndze <ndzekay@gmail.com>
2022-09-13 09:55:06 -0700

    Writing a function that prints lowecase alphabets followed new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  *main - Prints the alaphabet followed by a new line
+  *
+  *Retuns: 0 for success, 1 for failure
+  *
+  */
+void print_alphabet(void)
+{
+	int i;
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+_putchar('\n');
+}
+
+
+
+
