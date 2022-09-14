93b95fdb94f08335a46e4ad4ab3c4f1daf87dde3
Regina Ndze <ndzekay@gmail.com>
2022-09-14 05:32:39 -0700

    Adding a function that adds two intergers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *add - adds two intergers
+ *@a: int to add to b
+ *@b: int to add to a
+ *Return: sum of a and b
+*/
+
+int add(int a, int b)
+{
+		int c;
+
+		c = a + b;
+
+		return (c);
+}
+
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *main - check code
+  *
+  *Return: Always 0
+  */
+
+int main(void)
+{
+	add(3, 2);
+	return (0);
+}
