ca105945c59df6fd654e63d6e6041b6325d74681
Regina Ndze <ndzekay@gmail.com>
2022-09-28 12:50:31 -0700

    Addint a recrusinve factorial function

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  *factorial - returns the factoral of an int
+  *@n: int to find the factorial
+  *Return: int
+  *
+  */
+
+int factorial(int n)
+{
+	if (n == 0 || n == 1)
+	{
+	return (1);
+	}
+	else if (n < 0)
+	{
+	return (1);
+	}
+	else
+	{
+	return (n * factorial(n - 1));
+	}
+}
---
+++ 0x08-recursion/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *main -  checks the code
+  *Return: nothing
+  *
+  */
+
+int main(void)
+{
+	int r;
+
+	r = factorial(5);
+	printf("%d\n", r);
+	return (0);
+}
