f2cb35a63fd4f270b79615b1ececaf9f02885f0c
Regina Ndze <ndzekay@gmail.com>
2022-09-28 05:34:07 -0700

    Adding the new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,6 +7,11 @@
   */
 void _puts_recursion(char *s)
 {
+	if (s[0] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
 	if ((s[0] <= 122 && s[0] >= 97) ||
 	(s[0] <= 90 && s[0] >= 65) ||
 	(s[0] <= 53 && s[0] >= 32))
@@ -14,5 +19,4 @@ void _puts_recursion(char *s)
 		_putchar(s[0]);
 		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
 }
