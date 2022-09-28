05fbb0ac0db2e62c76d3db90a1bd04142c5ca27a
Regina Ndze <ndzekay@gmail.com>
2022-09-28 05:15:51 -0700

    Adding a fucntion that prints a string recursively

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *_puts_recursion - Function to print a string, followed by a new line
+  *@s: string to be printed
+  *return: always 0
+  */
+void _puts_recursion(char *s)
+{
+	if ((s[0] <= 122 && s[0] >= 97) ||
+	(s[0] <= 90 && s[0] >= 65) ||
+	(s[0] <= 53 && s[0] >= 32))
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+}
