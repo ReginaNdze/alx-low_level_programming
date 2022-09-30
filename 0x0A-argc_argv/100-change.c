0181e37549d26e0ff2866c357c733ffece6e99bd
Regina Ndze <ndzekay@gmail.com>
2022-09-30 13:20:48 -0700

    Task files

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - function that prints number of coins
+ * @argc: count
+ * @argv: number of arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+int pos, tot, change, aux;
+int coin[] = {25, 10, 5, 2, 1};
+pos = tot = change = aux = 0;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+tot = atoi(argv[1]);
+if (tot <= 0)
+{
+printf("0\n");
+return (0);
+}
+while (coin[pos] != '\0')
+{
+if (tot >= coin[pos])
+{
+tot -= coin[pos] * aux;
+}
+pos++;
+}
+printf("%d\n", change);
+return (0);
+}
