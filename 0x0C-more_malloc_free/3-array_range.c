ddf9e8a9d91dd4e1da9252e3d99be25f544fc8e8
Regina Ndze <ndzekay@gmail.com>
2022-10-06 15:32:13 -0700

    Adding task files

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * array_range - creates an array of int
+ * @min: minimum integer
+ * @max: maximum integer
+ * Return: array
+ */
+int *array_range(int min, int max)
+{
+int *num, k = 0, l = min;
+
+if (min > max)
+return (0);
+
+num = malloc((max - min + 1) * sizeof(int));
+
+if (num == NULL)
+return (0);
+
+while (k <= max - min)
+
+num[k++] = l++;
+
+return (num);
+
+}
