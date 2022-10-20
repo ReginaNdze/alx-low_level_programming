d0655a2e83b08f77ff87ebbc6174047b5ebe26d4
Regina Ndze <ndzekay@gmail.com>
2022-10-20 13:02:24 -0700

    Adding task files

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newn;
+	size_t newchar;
+
+	newn = malloc(sizeof(list_t));
+	if (newn == NULL)
+	return (NULL);
+
+	newn->str = strdup(str);
+
+	for (newchar = 0; str[newchar]; newchar++);
+
+	newn->len = newchar;
+	newn->next = *head;
+	*head = newn;
+
+	return (*head);
+}
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * add_node_end - adds a new node at the end
+ * of a list.
+ * @head: head of the linked list.
+ * @str: string
+ * Return: address of the head.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newn, *tmp;
+	size_t newchar;
+
+	newn = malloc(sizeof(list_t));
+	if (newn == NULL)
+	return (NULL);
+
+	newn->str = strdup(str);
+
+	for (newchar = 0; str[newchar]; newchar++)
+	;
+
+	newn->len = newchar;
+	newn->next = NULL;
+	tmp = *head;
+
+	if (tmp == NULL)
+	{
+	*head = newn;
+	}
+	else
+	{
+	while (tmp->next != NULL)
+	tmp = tmp->next;
+	tmp->next = newn;
+	}
+
+	return (*head);
+}
